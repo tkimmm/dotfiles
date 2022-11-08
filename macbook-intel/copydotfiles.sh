81f77d8e8c1c92a804db40725f78fa04a67975f9
Tom <tkimm@me.com>
2022-11-08 18:32:58 +0100

    add ease of use script for uploading

---
+++ macbook-intel/copydotfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ~/.vimrc ~/dotfiles/macbook-intel/
+cp ~/.zshrc ~/dotfiles/macbook-intel/
+cp ~/.config/alacritty/alacritty.yml ~/dotfiles/macbook-intel/
+cp ~/copydotfiles.sh ~/dotfiles/macbook-intel/

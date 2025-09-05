0d999631b1811fd7c97fd9c80260b12365474ce3
teekm@mbp2023 <tkimm@me.com>
2025-09-05 09:33:23 +0200

    add fish related config files

---
+++ clients/macbook-applesilicon/config.fish
@@ -0,0 +1,42 @@
+set fish_greeting
+set fish_cursor_default line
+set fish_cursor_insert line
+set fish_cursor_visual block
+set fish_cursor_replace underscore
+set fish_vi_force_cursor 1
+
+# Aliases can stay outside
+# Use fish_config for interactive fish configuration
+alias ma='/users/teekm/dev/2025/go/mindarc-aws/build/mindarcaws-darwin-amd64'
+alias ks=k9s
+alias q=exit
+alias v='nvim'
+alias c='clear'
+alias gg='lazygit'
+alias n='cd /Users/teekm/Dev/2025/vim; nvim'
+alias y='yazi'
+alias cd='z'
+
+function awsupdate
+    set -l myip (dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
+    set -l timestamp (date '+%Y-%m-%d %H:%M:%S')
+    aws ec2 modify-security-group-rules --group-id sg-0053cf18fac32bdbb --security-group-rules "SecurityGroupRuleId=sgr-0f36539805d442cb3,SecurityGroupRule={IpProtocol=tcp,FromPort=22,ToPort=22,CidrIpv4=$myip/32,Description='Tom Kim, updated at $timestamp'}"
+end
+
+# Interactive-only stuff
+if status is-interactive
+    fish_vi_key_bindings
+    # Key bindings
+    function fish_user_key_bindings
+        bind \cr 'history | fzf | read -l result; and commandline $result'
+    end
+
+    # Shell integrations
+    if string match -q -- 'tmux*' $TERM
+        set -g fish_vi_force_cursor 1
+    end
+
+    atuin init fish --disable-up-arrow | source
+    zoxide init fish | source
+    starship init fish | source
+end
